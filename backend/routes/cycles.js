b2429ac82c36960bb95ef74f521d36c7d5e2eba7
Abhisheak Sharma <zerodeaths693@gmail.com>
2023-03-28 01:00:13 +0530

    write routes and establish mongo connection

--- backend/routes/cycles.js
+++ backend/routes/cycles.js
@@ -1,32 +1,31 @@
-const express = require('express');
+const express = require("express");
 
 const router = express.Router();
 
 // GET all cycles
-router.get('/', (req, res) => {
-    res.json({msg: "GET all cycles"});
-})
+router.get("/", (req, res) => {
+  res.json({ msg: "GET all cycles" });
+});
 
 // GET a single cycle
-router.get('/:id', (req, res) => {
-    res.json({msg: "GET a single cycle"});
-})
+router.get("/:id", (req, res) => {
+  res.json({ msg: "GET a single cycle" });
+});
 
 // POST a new cycle
-router.post('/', (req, res) => {
-    res.json({msg: "POST a new cycle"});
-})
+router.post("/", (req, res) => {
+  res.json({ msg: "POST a new cycle" });
+});
 
 // DELETE a cycle
-router.delete('/:id', (req, res) => {
-    res.json({msg: "DELETE a cycle"});
-})
+router.delete("/:id", (req, res) => {
+  res.json({ msg: "DELETE a cycle" });
+});
 
 // UPDATE a cycle
-router.patch('/:id', (req, res) => {
-    res.json({msg: "UPDATE a cycle"});
-})
-
+router.patch("/:id", (req, res) => {
+  res.json({ msg: "UPDATE a cycle" });
+});
 
 module.exports = router;
 
@@ -34,4 +33,4 @@ module.exports = router;
 // POST /cycles - Creates a new cycle document
 // GET /cycles/:id - Gets a single cycle document
 // DELETE /cycles/:id - deletes a single cycle
-// PATCH/PUT /cycles/:id - updates a single cycle 
+// PATCH/PUT /cycles/:id - updates a single cycle
--- backend/server.js
+++ backend/server.js
@@ -1,7 +1,8 @@
-require('dotenv').config();
+require("dotenv").config();
 
-const express = require('express');
-const cycleRoutes = require('./routes/cycles');
+const express = require("express");
+const mongoose = require("mongoose");
+const cycleRoutes = require("./routes/cycles");
 
 // express app
 const app = express();
@@ -10,19 +11,22 @@ const app = express();
 app.use(express.json());
 
 app.use((req, res, next) => {
-    console.log(req.path, req.method);
-    next();
-})
+  console.log(req.path, req.method);
+  next();
+});
 
 // routes
-app.use('/api/cycles', cycleRoutes);
+app.use("/api/cycles", cycleRoutes);
 
-
-// listening to requests
-app.listen(process.env.PORT, () => {
-    console.log('listening on port', process.env.PORT);
-})
-
-
-
-
+// connect to db
+mongoose
+  .connect(process.env.MONGO_URI)
+  .then(() => {
+    // listen for requests
+    app.listen(process.env.PORT, () => {
+      console.log("connected to db and listening on port", process.env.PORT);
+    });
+  })
+  .catch((error) => {
+    console.log(error);
+  });
